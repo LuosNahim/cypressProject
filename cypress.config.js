e529c54685d7f739ad05e54a48c23251c6849eaa
An70 <antonionavapaz@outlook.com>
2023-12-24 23:45:51 -0400

    added linter - WIP configuration added CI - WIP Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
   baseUrl: "https://demoqa.com/",
+    projectId:"hs5ktd",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
